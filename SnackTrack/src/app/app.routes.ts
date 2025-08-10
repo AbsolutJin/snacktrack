d532e119d9a63ce58ea155c20b0d680292026617
maxschi22 <max.schimmel.1@web.de>
2025-08-10 21:45:01 +0200

    Create Modular base Structure

--- SnackTrack/src/app/app.routes.ts
+++ SnackTrack/src/app/app.routes.ts
@@ -10,4 +10,28 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard.page').then( m => m.DashboardPage)
+  },
+  {
+    path: 'inventory',
+    loadComponent: () => import('./pages/inventory/inventory.page').then( m => m.InventoryPage)
+  },
+  {
+    path: 'add-item',
+    loadComponent: () => import('./pages/add-item/add-item.page').then( m => m.AddItemPage)
+  },
+  {
+    path: 'item-detail',
+    loadComponent: () => import('./pages/item-detail/item-detail.page').then( m => m.ItemDetailPage)
+  },
+  {
+    path: 'account',
+    loadComponent: () => import('./pages/account/account.page').then( m => m.AccountPage)
+  },
+  {
+    path: 'tabs',
+    loadComponent: () => import('./pages/tabs/tabs.page').then( m => m.TabsPage)
+  },
 ];
