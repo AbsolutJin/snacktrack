615fe1d2aa76b9aaf06ab8cc51ad9b4bc45fc2fa
Artjom Maier <artjom.maier53@gmail.com>
2025-10-09 18:58:44 +0200

    Refactor: Update environment import in SupabaseClientService to use production configuration

--- SnackTrack/src/app/pages/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://bsqzxpjhmdohuyemhbcs.supabase.co/',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJzcXp4cGpobWRvaHV5ZW1oYmNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MzI2MDEsImV4cCI6MjA2NjQwODYwMX0.p4dC4vQet314ejbTNF2uVE_aqrp8rkCXZHyJlyShrRY',
-};
